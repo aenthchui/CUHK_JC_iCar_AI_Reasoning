340de7e198b45d9e90283c970852df7c76d13e88
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-09-28 11:27:14 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,11 @@
+//% color="#EABD00" weight=5 icon="\uf085" block="CUHK-JC iCar Extension"
+
+namespace CUHK_JC_iCar_Delivery{ 
+
+    /**
+     * This is a statement block 
+     */
+    //% block="Coming soon. Stay tuned!"
+    export function test(): void {
+    }
+}

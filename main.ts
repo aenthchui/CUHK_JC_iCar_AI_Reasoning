7b7e8a49b0acc2d5d548f4a80b760fb1d641f89a
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-09-30 17:58:33 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,39 @@
 //% color="#EC7482" weight=4 icon="\uf2e7" block="CUHK_JC_iCar_Delivery"
 
 namespace CUHK_JC_iCar_Delivery{ 
-
+  Current_Location = 0
   export enum reason {
       //% block="Skill-based"
-      skill,
+      skill = 1,
       //% block="Rule-based"
-      rule,
+      rule = 2,
       //% block="Knowledge-based"
-      knowledge
+      knowledge = 3
   }
   /**
   * Move iCar to array of points(A to H) using SKill, Rule or Knowledge-bases reasoning, click "+" to customize speed values
   */
-  //% block="iCar deliver food to $points using %index reasoning || at left speed %LSpeed\\%, right speed %RSpeed\\%, forward speed %FSpeed\\%"
+  //% block="iCar deliver food to $location using %index reasoning || at left speed %LSpeed\\%, right speed %RSpeed\\%, forward speed %FSpeed\\%"
   //% LSpeed.min=1 LSpeed.max=100 LSpeed.defl=20
   //% RSpeed.min=1 RSpeed.max=100 RSpeed.defl=20
   //% FSpeed.min=1 FSpeed.max=100 FSpeed.defl=20
   //% inlineInputMode=inline
   //% expandableArgumentMode="toggle"
-  export function startProgram(points?: string[], index?: reason, LSpeed?: number,RSpeed?: number,FSpeed?: number): void {
-    
+  export function startProgram(location: string[], index?: reason, LSpeed?: number,RSpeed?: number,FSpeed?: number): void {
+    if (input.buttonIsPressed(Button.A)){
+      huskylens.initI2c()
+      huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
+      Current_Location = 0
+      if(index == 1){
+      
+      }
+      if(index == 2){
+      
+      }
+      else{
+      
+      }
+    }
   }
     
   /**
